ede0f9993147d095aa46002d143bf0c02e6ddf69
Rik <hstarmans@gmail.com>
2021-06-07 16:24:09 +0200

    simulation now works

--- system.py
+++ system.py
@@ -5,18 +5,19 @@ from math import pi
 from pyoptools.raytrace.component import Component 
 from pyoptools.all import CylindricalLens, Ray, RectMirror, material, System, Plot3D
 from library import Polygon
-
+from analytical import Prism_properties
 
 class PrismScanner():
     '''defines a prism scanner
     '''  
-    def __init__(self, wavelength=405):
+    def __init__(self, wavelength=0.405):
         '''instantiate optical system
         
-        wavelength -- wavelength of the laser
+        wavelength -- wavelength of the laser in microns
         '''
         self.S = self.system()
         self.wavelength = wavelength
+        self.p = Prism_properties()
     
     def system(self, angle = 0):
         '''defines optical system using pyoptools
@@ -48,14 +49,14 @@ class PrismScanner():
 
         # Mirror
         M1=RectMirror(size=(10, 10, 2.0),
-                      reflectivity=1,
-                      material=material.schott["N-BK7"] )
+                      reflectivity=1)
 
         # Optical system
-        S=System(complist=[(CL_lens1,(0,0,0),(0.5*pi,0,0)),
-                           (prism,(0,10+15,0),(0,0,-np.radians(angle))),
-                           (CL_lens2,(0,50,0),(0.5*pi,0.5*pi,0)),
-                           (M1, (-10, 60, 0), (0.5*pi,0.5*pi, -0.25*pi))],n=1)
+        shift = 10
+        S=System(complist=[(CL_lens1,(0,shift,0),(0.5*pi,0,pi)),
+                           (prism,(0,10+15+shift,0),(0,0,-np.radians(angle))),
+                           (CL_lens2,(0,50+shift,0),(0.5*pi,0.5*pi, pi)),
+                           (M1, (-10, 60+shift, 0), (0.5*pi,0.5*pi, -0.25*pi))],n=1)
         return S
     
     def set_angle(self, angle = 0):
@@ -70,6 +71,8 @@ class PrismScanner():
                        wavelength=self.wavelength))
         self.S.propagate()
     
+    
+    
     def plot(self, angle = 0):
         ''' plot the system with a ray
         
@@ -81,17 +84,50 @@ class PrismScanner():
                       size=(250,100),
                       scale=3,
                       rot=[(np.radians(40), 0,0)])
-                
+    
+    def show_five_rays(self):
+        mirror_angles = self.find_mirror()
+        max_scan_angle = np.degrees(self.p.max_recommended_angle())
+        scan_angles = [-max_scan_angle, max_scan_angle, 0]
+        self.S.reset()
+        lst = mirror_angles + scan_angles
+        for angle in lst:
+            self.S.complist['C1'][-1][-1] = np.radians(angle)
+            self.S.ray_add(Ray(pos=[0,0,0],
+                               dir=[0,1,0], 
+                               wavelength=self.wavelength))
+            self.S.propagate()
+        return Plot3D(self.S,
+              center=(0,0,0),
+              size=(250,100),
+              scale=3,
+              rot=[(np.radians(40), 0,0)])
+    
     def find_mirror(self):
         ''' determines angle upon which mirror is hit
         ''' 
         hit_angle = []
-        failed = []
         mirror = self.S.complist['C3'][0]
-        for angle in range(-90, 90):
+        max_angle = int(round(np.degrees(self.p.max_angle_incidence())))
+        # course search
+        for angle in range(-max_angle, max_angle, 1):
             self.set_angle(angle)
             if len(mirror.hit_list): 
                 hit_angle.append(angle)
+        # optimize
+        low = min(hit_angle)
+        high = max(hit_angle)
+        new = True
+        while new:
+            self.set_angle(low-0.1)
+            if len(mirror.hit_list): 
+                low = low-0.1
             else:
-                failed.append(angle)
-        return hit_angle
+                new = False
+            self.set_angle(high+0.1)
+            if len(mirror.hit_list):
+                high = high+0.1
+                new = True
+            else:
+                new = False
+        return [low, high]
