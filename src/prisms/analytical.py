befb42f51b308e01ababc00e066dd50256a84250
Rik <hstarmans@gmail.com>
2021-06-09 00:18:01 +0200

    fixed flake8 messages

--- src/prisms/analytical.py
+++ src/prisms/analytical.py
@@ -1,11 +1,11 @@
 import numpy as np
 from scipy.integrate import dblquad
-from sympy import symbols, diff, cos, sin, sqrt
+from sympy import symbols, diff, sin, sqrt
 
 
 class Prism_properties:
     '''Key analytical properties in prism scanning
-     
+
        Source:
           Wyant: Basic Abberations and Optical testing
           http://rohr.aiax.de/BasicAberrationsandOpticalTesting.pdf
@@ -14,10 +14,10 @@ class Prism_properties:
         '''Instanstiates prism properties with dictionary
         '''
         self.set_params(params)
-        
+
     def set_params(self, params):
         '''set parameters with properties of scanner
-        
+
            In the dictionary several keys need to be defined
               n  -- refractive index
               facets -- number of polygon facets, should be even
@@ -31,19 +31,19 @@ class Prism_properties:
         '''
         if not params:
             params = {'n': 1.53,
-                     'facets': 4,
-                      'wavelength':405,
+                      'facets': 4,
+                      'wavelength': 405,
                       'T': 30,
                       'f_length': 75,
                       'd_bundle': 1.2,
                       'rot_hz': 350,
                       # apex angle is 1 arc_minute
-                      'apex_angle': 1/60 
+                      'apex_angle': 1/60
             }
         # f_number of the first cylindrical lens
         params['f_numb'] = params['f_length'] / params['d_bundle']
-        self.params = params         
-                 
+        self.params = params
+
     def spot_size(self, f_numb=None):
         '''returns the focused spot size of a Gaussian beam in microns
 
@@ -53,12 +53,12 @@ class Prism_properties:
            is used.
         '''
         if not f_numb:
-            f_numb=self.params['f_numb']
+            f_numb = self.params['f_numb']
         # Spot size
-        waist=2*self.params['wavelength']/np.pi*f_numb
-        waist/=1E3
+        waist = 2*self.params['wavelength']/np.pi*f_numb
+        waist /= 1E3
         return waist
-        
+
     def rayleigh_length(self, f_numb=None):
         '''returns the rayleigh length in mm
 
@@ -70,36 +70,36 @@ class Prism_properties:
            On default, the f-number of the first
            cylindrical lens is used.
         '''
-        waist=self.spot_size(f_numb)
-        rayleigh_length=np.pi*waist**2/self.params['wavelength']
+        waist = self.spot_size(f_numb)
+        rayleigh_length = np.pi*waist**2/self.params['wavelength']
         return rayleigh_length
-            
+
     def max_angle_incidence(self):
         '''returns maximum angle of incidence in radians
 
            If the prism is rotated further than this angle
            the next facet is hit.
         '''
-        utiltmax=np.radians(90-(180-360/self.params['facets'])/2) 
+        utiltmax = np.radians(90-(180-360/self.params['facets'])/2)
         return utiltmax
-    
+
     def max_recommended_angle(self, strehl_ratio=0.71):
         '''returns the maximum recommended angle of incidence
-        
+
            strehl ratio -- minimum required strehl ratio
         '''
-        utilt_max=self.max_angle_incidence()
-        iterations=1000
+        utilt_max = self.max_angle_incidence()
+        iterations = 1000
         for i in range(0, iterations):
-            angle=utilt_max/iterations*i
-            ratio=self.strehl_ratio(angle)
+            angle = utilt_max/iterations*i
+            ratio = self.strehl_ratio(angle)
             if ratio < strehl_ratio:
                 return utilt_max/iterations*(i-1)
-        
+
     def longitudinal_shift(self):
         '''return longitudinal shift in mm of focus bundle
 
-           The focal point of a focussed bundle, 
+           The focal point of a focussed bundle,
            with its focal point outside of the prism,
            is displaced.
            The prism in effect increases the focal length of the lens.
@@ -107,9 +107,9 @@ class Prism_properties:
         '''
         # Wyant page 41 equation 68
         params = self.params
-        slong=(params['n']-1)/params['n']*params['T']
+        slong = (params['n']-1)/params['n']*params['T']
         return slong
-               
+
     def transversal_shift(self, angle):
         '''transversal shift for a given angle of incidence
 
@@ -118,53 +118,56 @@ class Prism_properties:
            angle -- rotation angle of prism in radians
         '''
         # symbol needed for differentation
-        x=symbols('x') 
+        x = symbols('x')
         params = self.params
         T = params['T']
         n = params['n']
         x = angle
         # Wyant page 41 equation 70
-        expr=T*sin(x)*(1-sqrt((1-sin(x)**2)/(n**2-sin(x)**2)))
-        disp=expr.evalf(subs={x:angle})
+        expr = T*sin(x)*(1-sqrt((1-sin(x)**2)/(n**2-sin(x)**2)))
+        disp = expr.evalf(subs={x: angle})
         return disp
-    
+
     def cross_scan_error(self, focal_distance=35):
-        '''error orthogonal to scanline 
-            
+        '''error orthogonal to scanline
+
            the sides of the prism are not perfectly parallel
            this result in a cross-scan error, i.e.
            error orthogonal to the scanline.
            There is also an error
-           parallel to the scanline, this can be resolved 
-           with interpolation as it only changes the size 
+           parallel to the scanline, this can be resolved
+           with interpolation as it only changes the size
            and speed along the the scanline.
 
            Note: If a mirror has a facet to datum error of
            one degree, the total error is two degrees.
 
-           focal distance  -- distance between focal point 
+           focal distance  -- distance between focal point
                               and edge prism
         '''
         # https://en.wikipedia.org/wiki/Prism
         # assumes incidence angle and apex angle are both small
         #  the apex angle is small as adjacent sides are almost parallel
-        #  the angle of incidicence small in the orthogonal case 
+        #  the angle of incidicence small in the orthogonal case
         #  but not the parallel case
         #  furthermore both sides will have this
         params = self.params
         d_angle = (params['n']-1)*np.radians(params['apex_angle'])
         cross_error = np.tan(d_angle)*focal_distance
         # a difference with reflective polygon scanning is that you
-        # hit 2 sides and not one, in the above it is assumed one side is perfect
-        # you might fix this by counting this as a tilted parallel plate + 
+        # hit 2 sides and not one, in the above
+        # it is assumed one side is perfect
+        # you might fix this by counting
+        # this as a tilted parallel plate +
         # cross scan error
         # the current fix is multiplication by 2
-        print("WARNING: It is between 0.5 and 1 of this error, see comments in code.")
+        print("WARNING: It is between 0.5 and 1 of this error" +
+              " see comments in code.")
         cross_error *= 2
         return cross_error
 
     def print_properties(self):
-        '''gives a print out of defining properties 
+        '''gives a print out of defining properties
            of the optical system
         '''
         T = self.params['T']
@@ -180,42 +183,47 @@ class Prism_properties:
         #  y=f(x) dx/dt=c   x=ct
         #    here y is the position of the spot in one dimension measured in mm
         #         x is the angle as a position of the time
-        #         c is the angular rotation speed of the prism (assumed constant)
-        #  --> dy/dt=dy/dx(x(t))*c 
-        x=symbols('x') 
+        #         c is the angular rotation speed of
+        #         the prism (assumed constant)
+        #  --> dy/dt=dy/dx(x(t))*c
+        x = symbols('x')
         # Wyant page 41 equation 70
-        expr=T*sin(x)*(1-sqrt((1-sin(x)**2)/(n**2-sin(x)**2)))
-        sdisp=diff(expr, x)
+        expr = T*sin(x)*(1-sqrt((1-sin(x)**2)/(n**2-sin(x)**2)))
+        sdisp = diff(expr, x)
         # fractional speed
-        fraction=sdisp.evalf(subs={x:0})/sdisp.evalf(subs={x:utilt})
+        fraction = sdisp.evalf(subs={x: 0})/sdisp.evalf(subs={x: utilt})
         print(f"The speed at the center is {round(fraction*100,2)}" +
-                " % of the speed at the edges.")
+              " % of the speed at the edges.")
         ang_speed = np.pi*2*self.params['rot_hz']
-        print(f"The speed at the edges is " +
+        print("The speed at the edges is " +
               f"{sdisp.evalf(subs={x:utilt})/1000*ang_speed:.2f} m/s.")
-        print(f"The spot radius of the first cylindical lens is {self.spot_size():.2f}"
-                + " micrometers.")
+        print("The spot radius of the first cylindical lens is " +
+              f"{self.spot_size():.2f} micrometers.")
         print(f"The Rayleigh range is {self.rayleigh_length():.3f} mm.")
-        print(f"The Strehl ratio is {self.strehl_ratio(utilt, verbose=True):.2f}")
-        print(f"The cross scan error is {self.cross_scan_error()*1000:.2f} microns")
-      
+        print("The Strehl ratio is" +
+              f"{self.strehl_ratio(utilt, verbose=True):.2f}")
+        print("The cross scan error is" +
+              f"{self.cross_scan_error()*1000:.2f} microns")
+
     def strehl_ratio(self, utilt, verbose=False):
         '''returns the strehl ratio of the optical system
-        
-           These calculation only account for the distortions caused by the 
+
+           These calculation only account for the distortions caused by the
            the prism.
-           The optical performance of the system can be expressed with the Strehl ratio. 
-           If the Strehl ratio gets below a threshold,
-           the aberrations will become dominant and the system will not image properly.
-           As a result, it must be ensured via calculation that the Strehl ratio is larger
+           The optical performance of the system can be expressed with
+           the Strehl ratio. If the Strehl ratio gets below a threshold,
+           the aberrations will become dominant and the system
+           will not image properly. As a result, it must be ensured via
+           calculation that the Strehl ratio is larger
            than some acceptable limit, e.g. the Rayleigh limit of 0.71.
-           Literature provides us with the Seidel coefficients of the main aberrations. 
-           These are used to determine the Strehl ratio for a given rotation angle.
-           In some literature, the Strehl ratio is referred to as the strehl radius
-        
+           Literature provides us with the Seidel coefficients
+           of the main aberrations These are used to determine the Strehl
+           ratio for a given rotation angle. In some literature,
+           the Strehl ratio is referred to as the strehl radius
+
            utilt -- tilt angle in radians
            verbose -- prints lambda opd rms if True
-        ''' 
+        '''
         params = self.params
         wavelength = params['wavelength']
         fnumber = params['f_numb']
@@ -223,42 +231,49 @@ class Prism_properties:
         T = params['T']
         # 3rd order Seidel aberrations in [mm]
         #  Wyant page 42 equation 72; spherical aberration
-        sabr=-T/pow(fnumber,4)*((pow(n,2)-1)/(128*pow(n,3)))
+        sabr = -T/pow(fnumber, 4)*((pow(n, 2)-1)/(128*pow(n, 3)))
         #   Wyant page 44 equation 75; coma
-        #   Note: cosine has been omitted, will be added back when function f is defined
-        coma=-T*utilt/pow(fnumber,3)*((pow(n,2)-1)/(16*pow(n,3)))
+        #   Note: cosine has been omitted, will be added back when function
+        #         f is defined
+        coma = -T*utilt/pow(fnumber, 3)*((pow(n, 2)-1)/(16*pow(n, 3)))
         #   Wyant page 45 equation 77 ; astigmatism
-        #   Note: cosine has been omitted, will be added back when function f is defined
-        astig=-T*pow(utilt,2)/pow(fnumber,2)*((pow(n,2)-1)/(8*pow(n,3)))
-        # To calculate the combined RMS we add back the functional forms to the coefficients
+        #   Note: cosine has been omitted, will be added back when function
+        #         f is defined
+        astig = -T*pow(utilt, 2)/pow(fnumber, 2)*((pow(n, 2)-1)/(8*pow(n, 3)))
+        # To calculate the combined RMS we add back the functional
+        # forms to the coefficients
         # They have been obtained from Wyant, page 17, table 2
         # The function f defines the wavefront aberration denoted by Wyant as w
-        def f(theta,rho):
-            return sabr*(rho**4)+astig*(rho**2)*(np.cos(theta)**2)+coma*(rho**3)*np.cos(theta)
+
+        def f(theta, rho):
+            return (sabr*(rho**4)+astig*(rho**2) *
+                    (np.cos(theta)**2)+coma*(rho**3)*np.cos(theta))
         # We now try to evaluate equation 62, page 37, Wyant
         # First we define two auxiliary functions
-        def ws(theta,rho):
-            return f(theta,rho)**2*rho
-        def w(theta,rho):
-            return f(theta,rho)*rho
+
+        def ws(theta, rho):
+            return f(theta, rho)**2*rho
+
+        def w(theta, rho):
+            return f(theta, rho)*rho
         # Hereafter we evaluate the integral, i.e. equation 62
-        var=1/np.pi*dblquad(ws,0,1,lambda rho: 0, lambda rho: 2*np.pi)[0]\
-        -1/(np.pi**2)*(dblquad(w,0,1,lambda rho: 0, lambda rho: 2*np.pi)[0]**2)
+        var = (1/np.pi*dblquad(ws, 0, 1, lambda rho: 0, lambda rho: 2*np.pi)[0]
+               - 1/(np.pi**2) *
+               (dblquad(w, 0, 1, lambda rho: 0, lambda rho: 2*np.pi)[0]**2))
         # the RMS value is obtained by the root
-        rms=np.sqrt(var)
-        # The lambda RMS is expressed in [mm] 
+        rms = np.sqrt(var)
+        # The lambda RMS is expressed in [mm]
         # and will be converted to wavelength units
-        lambdarms=rms/(wavelength*1E-6)
+        lambdarms = rms/(wavelength*1E-6)
         if verbose:
             print(f"The lambda OPD RMS is {lambdarms:6f}")
-        # The Strehl radius is calculated with the first 
+        # The Strehl radius is calculated with the first
         # three terms of its Taylor series
         # Wyant page 39 equation 67
-        rstrehl=1-(2*np.pi*lambdarms)**2+(2*np.pi*lambdarms)**4/2
+        rstrehl = 1-(2*np.pi*lambdarms)**2+(2*np.pi*lambdarms)**4/2
         return rstrehl
 
-    
+
 if __name__ == "__main__":
     p = Prism_properties()
     p.print_properties()
-    
--- src/prisms/system.py
+++ src/prisms/system.py
@@ -1,38 +1,39 @@
 from math import pi
 
 import numpy as np
-from pyoptools.raytrace.component import Component 
-from pyoptools.all import CylindricalLens, Ray, RectMirror, material, System, Plot3D
+from pyoptools.all import (CylindricalLens, Ray, RectMirror,
+                           material, System, Plot3D)
 
 from prisms.library import Polygon
 from prisms.analytical import Prism_properties
 
 
 class PrismScanner():
-    '''defines a prism scanner
-    '''  
+    '''defines a prism scanner'''
     def __init__(self, wavelength=0.405):
         '''instantiate optical system
-        
+
         wavelength -- wavelength of the laser in microns
         '''
         self.S = self.system()
         self.wavelength = wavelength
         self.p = Prism_properties()
-    
-    def system(self, angle = 0):
+
+    def system(self, angle=0):
         '''defines optical system using pyoptools
-        
+
         System is ordered upon distance from optical source.
         Closest component comes first.
-        
+
         angle -- angle in degrees of prism
         '''
-        # Cylinder LENS 1 Edmund optics 68-048 
+        # Cylinder LENS 1 Edmund optics 68-048
         # BFL + CT = 73.68 + 2 = 75.68
-        CL_lens1=CylindricalLens(size=(12.5, 25), thickness=2,
-                                 curvature_s1=1./75.68, curvature_s2=0,
-                                 material=material.schott["N-BK7"])
+        CL_lens1 = CylindricalLens(size=(12.5, 25),
+                                   thickness=2,
+                                   curvature_s1=1./75.68,
+                                   curvature_s2=0,
+                                   material=material.schott["N-BK7"])
 
         # Prism
         prism = Polygon(sides=4,
@@ -42,50 +43,49 @@ class PrismScanner():
 
         # Cylinder LENS 2 Edmund optics 68-046
         # BFL + CT = 23.02 + 3
-        CL_lens2=CylindricalLens(size=(12.5, 25),
-                                 thickness=3,
-                                 curvature_s1=1./26.02,
-                                 curvature_s2=0,
-                                 material=material.schott["N-BK7"])
+        CL_lens2 = CylindricalLens(size=(12.5, 25),
+                                   thickness=3,
+                                   curvature_s1=1./26.02,
+                                   curvature_s2=0,
+                                   material=material.schott["N-BK7"])
 
         # Mirror
-        M1=RectMirror(size=(10, 10, 2.0),
-                      reflectivity=1)
+        M1 = RectMirror(size=(10, 10, 2.0),
+                        reflectivity=1)
 
         # Optical system
         shift = 10
-        S=System(complist=[(CL_lens1,(0,shift,0),(0.5*pi,0,pi)),
-                           (prism,(0,10+15+shift,0),(0,0,-np.radians(angle))),
-                           (CL_lens2,(0,50+shift,0),(0.5*pi,0.5*pi, pi)),
-                           (M1, (-10, 60+shift, 0), (0.5*pi,0.5*pi, -0.25*pi))],n=1)
+        complist = [(CL_lens1, (0, shift, 0), (0.5*pi, 0, pi)),
+                    (prism, (0, 10+15+shift, 0), (0, 0, -np.radians(angle))),
+                    (CL_lens2, (0, 50+shift, 0), (0.5*pi, 0.5*pi, pi)),
+                    (M1, (-10, 60+shift, 0), (0.5*pi, 0.5*pi, -0.25*pi))]
+        S = System(complist=complist, n=1)
         return S
-    
-    def set_angle(self, angle = 0):
-        ''' sets the angle of the prism to a given angle
-        
-            angle -- rotation angle in degrees of prism
-        ''' 
+
+    def set_angle(self, angle=0):
+        '''sets the angle of the prism to a given angle
+
+           angle -- rotation angle in degrees of prism
+        '''
         self.S.complist['C1'][-1][-1] = np.radians(angle)
         self.S.reset()
-        self.S.ray_add(Ray(pos=[0,0,0],
-                       dir=[0,1,0], 
+        self.S.ray_add(Ray(pos=[0, 0, 0],
+                       dir=[0, 1, 0],
                        wavelength=self.wavelength))
         self.S.propagate()
-    
-    
-    
-    def plot(self, angle = 0):
+
+    def plot(self, angle=0):
         ''' plot the system with a ray
-        
+
             angle -- rotation angle in degrees of prism
-        ''' 
+        '''
         self.set_angle(angle)
         return Plot3D(self.S,
-                      center=(0,0,0),
-                      size=(250,100),
+                      center=(0, 0, 0),
+                      size=(250, 100),
                       scale=3,
-                      rot=[(np.radians(40), 0,0)])
-    
+                      rot=[(np.radians(40), 0, 0)])
+
     def show_five_rays(self):
         mirror_angles = self.find_mirror()
         max_scan_angle = np.degrees(self.p.max_recommended_angle())
@@ -94,26 +94,26 @@ class PrismScanner():
         lst = mirror_angles + scan_angles
         for angle in lst:
             self.S.complist['C1'][-1][-1] = np.radians(angle)
-            self.S.ray_add(Ray(pos=[0,0,0],
-                               dir=[0,1,0], 
+            self.S.ray_add(Ray(pos=[0, 0, 0],
+                               dir=[0, 1, 0],
                                wavelength=self.wavelength))
             self.S.propagate()
         return Plot3D(self.S,
-              center=(0,0,0),
-              size=(250,100),
-              scale=3,
-              rot=[(np.radians(40), 0,0)])
-    
+                      center=(0, 0, 0),
+                      size=(250, 100),
+                      scale=3,
+                      rot=[(np.radians(40), 0, 0)])
+
     def find_mirror(self):
         ''' determines angle upon which mirror is hit
-        ''' 
+        '''
         hit_angle = []
         mirror = self.S.complist['C3'][0]
         max_angle = int(round(np.degrees(self.p.max_angle_incidence())))
         # course search
         for angle in range(-max_angle, max_angle, 1):
             self.set_angle(angle)
-            if len(mirror.hit_list): 
+            if len(mirror.hit_list):
                 hit_angle.append(angle)
         # optimize
         low = min(hit_angle)
@@ -121,7 +121,7 @@ class PrismScanner():
         new = True
         while new:
             self.set_angle(low-0.1)
-            if len(mirror.hit_list): 
+            if len(mirror.hit_list):
                 low = low-0.1
             else:
                 new = False
